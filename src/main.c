c0831f5695d491b5f8618104d21d729b0a7a1584
h-custodio <hprince0814@gmail.com>
2026-03-22 23:39:31 -0700

    Added exit option to REPL

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
     command[strcspn(command, "\n")] = '\0';
 
     if (strcmp(command, "exit") == 0) {
-      exit(1);
+      exit(0);
     } else {
       printf("%s: command not found", command);
       printf("\n");
