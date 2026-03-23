a8a4da46072fbdaf7563d3d65c3aeaf2f3a7c62a
h-custodio <hprince0814@gmail.com>
2026-03-23 02:00:40 -0700

    Introduced echo command option

--- src/main.c
+++ src/main.c
@@ -2,37 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 
+int findDelimIndex (const char *input) {
+  return strcspn(input, " ");
+}
+
+// Extract the first argument (input)
+char* extractArg(const char *input, int delim_index) {
+  // Initialize a dynamic char array to hold our string; Return NULL if NULL
+  char *com_arg = malloc(delim_index  + 1);
+  if (!com_arg) return NULL;
+
+  // Extracts the first word from input
+  strncpy(com_arg, input, delim_index);
+  // End string with null terminator
+  com_arg[delim_index] = '\0';
+
+  return com_arg;
+}
+
+// Prints the arguments
+void printArgList(const char *input, int delim_index) {  
+  char arg_list[100] = "";
+  strncat(arg_list, input + delim_index + 1, strlen(input));
+  printf(arg_list);
+  printf("\n");
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   //ongoing Read-Eval-Print Loop
-  while (true) {
+  while (1) {
     printf("$ ");
 
-    // Initialize input variable and take user input
-    char command[100];
-    fgets(command, sizeof(command), stdin);
-    //remove trailing newline
-    command[strcspn(command, "\n")] = '\0';
+    // Initialize input variable, then takes user input and removes trailing newline
+    char input[100];
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = '\0';
     
-    //extracts the command to check
-    int space_index = strcspn(command, " ");
-    char com_arg[16] = "";
-    strncat(com_arg, command, space_index);
+    //finds the first argument(command)
+    char *command = extractArg(input, findDelimIndex(input));
 
-    if (strcmp(command, "exit") == 0) {
-      exit(0);
-    } else if (strcmp(com_arg, "echo") == 0) {
-      char arg_list[100] = "";
-      strncat(arg_list, command + space_index + 1, strlen(command));
-      printf(arg_list);
-      printf("\n");
+    // IF tree
+    if (strcmp(command, "type") == 0) {
+      char *argument = extractArg(command, findDelimIndex(command)); 
+      if (strcmp(argument, "echo") == 0) {
+        printf("%s is a shell builtin\n", argument);
+      } else if (strcmp(argument, "exit") == 0) {
+          printf("%s is a shell builtin\n", argument);
+      } else {
+          printf("%s: input not found\n", argument);
+      }
+      free(argument);
+    } else if (strcmp(input, "exit") == 0) {
+        exit(0);
+    } else if (strcmp(command, "echo") == 0) {
+        printArgList(input, findDelimIndex(input));
     } else {
-      printf("%s: command not found", command);
-      printf("\n");
+        printf("%s: input not found\n", input);
     }
-  }
 
+    free(command);
+  }
   return 0;
 }
