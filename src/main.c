b6bdd5336784599a9994465ca3c44d9b62ceacc7
h-custodio <hprince0814@gmail.com>
2026-03-22 20:06:07 -0700

    handle invalid commands

--- src/main.c
+++ src/main.c
@@ -8,5 +8,15 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
 
+  // Initialize a char type variable named 'str' with the size 100 to as as a string
+  char input* = malloc(100);
+  fgets(input, sizeof(input), stdin);
+
+  if (input == input) {
+    printf("{command}: command not found")
+  }
+  
+
+
   return 0;
 }
