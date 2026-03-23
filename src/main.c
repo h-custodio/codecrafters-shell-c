fd746a0b0162ae6e0e951ab9c1596b74d25849c0
h-custodio <hprince0814@gmail.com>
2026-03-22 23:45:36 -0700

    Introduced echo command option

--- src/main.c
+++ src/main.c
@@ -18,11 +18,10 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(command, "exit") == 0) {
       exit(0);
-    } else if (strcmp(command, "echo") == 0) {
+    } else if (strcmp(command, "echo ") == 0) {
       printf(command);
     } else {
-      printf("1");
-      //printf("%s: command not found", command);
+      printf("%s: command not found", command);
       printf("\n");
     }
   }
